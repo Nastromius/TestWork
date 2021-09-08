dfec4137bc6369c7c461b401d133ee1beef8bf5b
Vladimir <86318922+Nastromius@users.noreply.github.com>
2021-09-08 12:57:50 +0300

    Add files via upload

---
+++ Linker.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IMPLCLASS
+{
+    interface AbstractMeasure
+    {
+        int FigureArea();
+    }
+
+    
+    abstract class Component
+    {
+        protected Random random = new Random();
+        public abstract string Operation();
+
+        public virtual void Add(Component cmp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(Component cmp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+        public virtual int FigureArea()
+        {
+            int m_randomValue = 0;
+            return m_randomValue;
+        }
+    }
+    class Group : Component
+    {
+        private int m_iSumAreas;
+
+        public Group()
+        {
+            m_iSumAreas = 0;
+        }
+        
+        protected List<Component> _children = new List<Component>();
+
+        public override void Add(Component m_cmp)
+        {
+            this._children.Add(m_cmp);
+        }
+
+        public override void Remove(Component m_cmp)
+        {
+            this._children.Remove(m_cmp);
+        }
+
+        public override string Operation()
+        {
+            int m_i = 0;
+            string m_result = "Group(";
+
+            foreach (Component m_cmp in this._children)
+            {
+                m_result += m_cmp.Operation();
+                if (m_i != this._children.Count - 1)
+                {
+                    m_result += "+";
+                }
+                m_i++;
+            }
+
+            return m_result + ")";
+        }
+
+        public void CalcMeasure()
+        {
+
+            foreach (Component m_cmp in this._children)
+            {
+                m_iSumAreas += m_cmp.FigureArea();
+            }
+        }
+
+        public int GetSumAreas()
+        {
+            return m_iSumAreas;
+        }
+
+    }
+    class Figure : Component, AbstractMeasure
+    {
+        public override int FigureArea()
+        {
+            int m_randomValue = random.Next(10, 100);
+            return m_randomValue;
+        }
+
+        public override string Operation()
+        {
+            return "Figure";
+        }
+
+        public override bool IsComposite()
+        {
+            return false;
+        }
+    }
+    class Client
+    {
+        public void ClientCode(Component m_cmp)
+        {
+            Console.WriteLine($"RESULT: {m_cmp.Operation()}\n");
+        }
+
+        public void ClientCode2(Component m_cmp1, Component m_cmp2)
+        {
+            if (m_cmp1.IsComposite())
+            {
+                m_cmp1.Add(m_cmp2);
+            }
+
+            Console.WriteLine($"RESULT: {m_cmp1.Operation()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using IMPLCLASS;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+        Figure quadrateOne = new Figure();
+        Figure quadrateTwo = new Figure();
+        Figure quadrateThree = new Figure();
+
+        Figure circleeOne = new Figure();
+        Figure circleeTwo = new Figure();
+        Figure circleeThree = new Figure();
+
+        Figure triangleOne = new Figure();
+        Figure triangleTwo = new Figure();
+        Figure triangleThree = new Figure();
+
+        Group Quadrates = new Group();
+        Group Circlees = new Group();
+        Group Triangles = new Group();
+
+        Group GroupFigures = new Group();
+
+        Quadrates.Add(quadrateOne);
+        Quadrates.Add(quadrateTwo);
+        Quadrates.Add(quadrateThree);
+
+        Circlees.Add(circleeOne);
+        Circlees.Add(circleeTwo);
+        Circlees.Add(circleeThree);
+
+        Triangles.Add(triangleOne);
+        Triangles.Add(triangleTwo);
+        Triangles.Add(triangleThree);
+
+        GroupFigures.Add(Quadrates);
+        GroupFigures.Add(Circlees);
+
+        client.ClientCode(GroupFigures);
+        client.ClientCode2(GroupFigures, Triangles);
+
+        Quadrates.CalcMeasure();
+        Console.WriteLine("\nСумма площадей квадратов: " + Quadrates.GetSumAreas());
+
+        Circlees.CalcMeasure();
+        Console.WriteLine("Сумма площадей окружностей: " + Circlees.GetSumAreas());
+
+        Triangles.CalcMeasure();
+        Console.WriteLine("Сумма площадей треугольников: " + Triangles.GetSumAreas());
+
+
+    }
+}
